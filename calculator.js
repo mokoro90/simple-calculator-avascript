0a814fb7fa67f5b51ca9fc95d5b766f4c5357ed9
mokoro90 <53904650+mokoro90@users.noreply.github.com>
2022-06-24 13:26:56 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply (a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a + b
+}
+
+switch(x){
+    case 'add':
+        add(a, b);
+        break;
+    case 'subtract':
+        subtract(a, b);
+        break;
+    
+    case 'multiply':
+        multiply(a, b);
+        break;
+
+    case 'multiply':
+        multiply(a, b);
+        break;
+    default:
+        console.log("error option no available")
+
+}
